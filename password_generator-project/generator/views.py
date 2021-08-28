cbeac1972f845c9d76437e2489258f4093127c87
IainTarrant <iain@idt.net.au>
2021-08-28 13:17:58 +1000

    Modified project to use a single page and store form choices for resending to form at render time

--- password_generator-project/generator/views.py
+++ password_generator-project/generator/views.py
@@ -3,23 +3,26 @@ import random
 
 # Create your views here.
 def home(request):
-    return render(request, 'generator/home.html')
+    if request.POST:
+        character_str = 'abcdefghijklmnopqrstuvwxyz'
+        characters = list(character_str)
+        uppercase, special, numbers = False, False, False
+        if request.POST.get('uppercase'):
+            characters.extend(list(character_str.upper()))
+            uppercase = True
+        if request.POST.get('special'):
+            characters.extend(list('!@#$%^&*()'))
+            special = True
+        if request.POST.get('numbers'):
+            characters.extend(list('1234567890'))
+            numbers = True
 
+        length = int(request.POST.get('length', 12))
+        thepassword = ''
+        for x in range(length):
+            thepassword += random.choice(characters)
+        return render(request, 'generator/home.html', {'password':thepassword, 'uppercase':uppercase,
+                                                       'special':special, 'numbers':numbers, 'length':length})
+    else:
+        return render(request, 'generator/home.html')
 
-def password(request):
-    character_str = 'abcdefghijklmnopqrstuvwxyz'
-    characters = list(character_str)
-
-    if request.GET.get('uppercase'):
-        characters.extend(list(character_str.upper()))
-    if request.GET.get('special'):
-        characters.extend(list('!@#$%^&*()'))
-    if request.GET.get('numbers'):
-        characters.extend(list('1234567890'))
-
-    length = int(request.GET.get('length', 12))
-    thepassword = ''
-    for x in range(length):
-        thepassword += random.choice(characters)
-
-    return render(request, 'generator/password.html', {'password':thepassword})
